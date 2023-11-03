5fce3d7cf309c3351b73a836c9b31ebce69db596
jam43040 <107657927+jam43040@users.noreply.github.com>
2023-11-03 18:27:35 -0400

    Made server connection work

--- demo.js
+++ demo.js
@@ -6,7 +6,9 @@ async function main(){
     const client = new MongoClient(uri);
 
     try {
+        console.log("ATTEMPT");
         await client.connect();
+        await listDatabases(client);
     } catch (e) {
         console.error(e);
     } finally {
@@ -14,6 +16,14 @@ async function main(){
     }
 
 
-main().catch(console.error);
+
+
+async function listDatabases(client){
+    const databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db=>{console.log(`- ${db.name}`);})
 
 }
+
+}
+main().catch(console.error);
