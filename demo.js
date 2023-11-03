0c494493d5874629af520a9b9174bdf857f29722
BennettGN <bennettgodinhonelson@gmail.com>
2023-11-03 19:38:30 -0400

    Fixed Typos in Express Code

--- demo.js
+++ demo.js
@@ -1,56 +1,51 @@
-//Import Libraries
-const {MongoClient} = require('mongodb');
+// Import Libraries
+const { MongoClient } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-//Create a New Express Application
+// Create a New Express Application
 const app = express();
 
-//Parse application/json
+// Parse application/json
 app.use(bodyParser.json());
 
-async function main(){
+async function main() {
     const uri = "mongodb+srv://demo:demopassword@cluster0.niuy4mb.mongodb.net/?retryWrites=true&w=majority";
 
     const client = new MongoClient(uri);
-    const db = client.db();
     try {
-        console.log("ATTEMPT");
         await client.connect();
         await listDatabases(client);
     } catch (e) {
         console.error(e);
-    } finally {
-        await client.close();
     }
 
-    //Post Request for signing up new user
-    app.post('/api/send-password',(req,res)=>{
+    // Post Request for signing up a new user
+    app.post('/api/send-password', (req, res) => {
         const dataToInsert = req.body;
-        const usersCollection = db.collection('Users');
+        const usersCollection = client.db().collection('Users');
 
-        usersCollection.insertOne(dataToInsert,(err,result)=>{
-            if(err){
-                console.err('Error Signing Student Up',err);
+        usersCollection.insertOne(dataToInsert, (err, result) => {
+            if (err) {
+                console.error('Error Signing User Up', err);
                 res.status(500).send('Error inserting data into User Database');
-            }else{
+            } else {
                 res.status(200).send('User Entered Successfully');
             }
-        })
-
+        });
     });
 
+    app.listen(3000, () => {
+        console.log("Server is listening");
+    });
+}
 
-
-
-async function listDatabases(client){
+async function listDatabases(client) {
     const databasesList = await client.db().admin().listDatabases();
     console.log("Databases:");
-    databasesList.databases.forEach(db=>{console.log(`- ${db.name}`);})
+    databasesList.databases.forEach(db => {
+        console.log(`- ${db.name}`);
+    });
+}
 
-}
-    app.listen(3000,()=>{
-        console.log("Server is listening");
-    })
-}
 main().catch(console.error);
