119f43254b054c3cf4257da9a97ed15f514cd94c
BennettGN <bennettgodinhonelson@gmail.com>
2023-11-03 19:18:04 -0400

    Created Signup Behavior Button

---
+++ client/src/signup_behavior.js
@@ -0,0 +1,32 @@
+const signupButton = document.getElementById('sign-in-button');
+
+signupButton.onclick() = () =>{
+    //Gets Element From Button Form
+    const firstName = document.getElementById('firstNameInput').value;
+    const lastName = document.getElementById('lastNameInput').value;
+    const password = document.getElementById('passwordInput').value;
+    const email = document.getElementById('emailInput').value;
+    const isValidPassword = false;
+
+    const request = {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+
+        params: JSON.stringify({
+            firstName : firstName,
+            lastName : lastName,
+            email: email,
+            Admin: false,
+            Tokens: 0,
+            Courses: [],
+            password: password
+        })
+    }
+    //Do function for checking if password is valid
+    
+    if(isValidPassword){
+        // send the request to the server
+        fetch('/api/send-password', request);
+    }
+
+}
