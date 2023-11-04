14a0eed11feefbfc651dda9620377e6cd3dbc26b
danielburke03 <dlburke08@gmail.com>
2023-11-04 01:55:03 -0400

    Added CSS.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import AdminHome from './/pages/AdminHome';
 import StudentHome from './pages/StudentHome';
+import './App.css'
 
 
 function App() {
     
     return (
         <div className="App">
-            <h1>Student Home Page</h1>
-            <StudentHome />
+            <div className='header'>
+                <h1>CTRL-R</h1>
+                <h3>Welcome!</h3>
+            </div>
+            <div className='body'>
+                <h1>Student Home Page</h1>
+                <AdminHome />
+            </div>
         </div>
     )
 }
