c34cad64cb5fda72ef07cd06a8acf37d392c395a
BennettGN <bennettgodinhonelson@gmail.com>
2023-11-04 05:07:27 -0400

    Made the Database an Array of Arrays

--- server/data.js
+++ server/data.js
@@ -1,9 +1,7 @@
 //Data Points Used in Demo of Application
 
-const JohnDoeDataPoint = ["Jane","Doe",false,103,"Math",95,"3pN23z3H","janedoe@gmail.com"];
-const SteveJobsDataPoint = ["Steve","Jobs",true,0,"None",0,"l3c7Y7215","stevejobs@gmail.com"];
+const database = [["Jane","Doe",false,103,"Math",95,"3pN23z3H","janedoe@gmail.com"],["Steve","Jobs",true,0,"None",0,"l3c7Y7215","stevejobs@gmail.com"]];
 
 module.exports = [
-    JohnDoeDataPoint,
-    SteveJobsDataPoint
+    database
 ]
