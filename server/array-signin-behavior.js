494e314ed7f833c952fe5e6122046e5a852cdacc
JacksonBoes <jackson.boes02@gmail.com>
2023-11-04 06:02:33 -0400

    wrote data file

--- server/array-signin-behavior.js
+++ server/array-signin-behavior.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function SignInButton(email, password) {
-        const database = readUserData('Users.txt');
+        const database = readUserData('server/Users.txt');
         for (const i = 0; i < database.length; i++) {
             if (database[i][7] === email && database[i][6] === password) {
                 return true;
