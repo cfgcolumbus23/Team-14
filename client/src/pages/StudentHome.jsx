401c028e4c8ce2a27c3c127ddb135cbc51c59842
jam43040 <107657927+jam43040@users.noreply.github.com>
2023-11-04 01:15:31 -0400

    Progress bar implementation

--- client/src/pages/StudentHome.jsx
+++ client/src/pages/StudentHome.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import './StudentHome.css';
 
 const StudentHome = ( {courses} ) => {
-    courses = {};
-    courses['Basic Computer Skills'] = 'www.google.com';
-    courses['Internet Basics'] = 'www.youtube.com';
-    courses['Using Email'] = 'www.instagram.com';
+    courses = {}
+    courses['Northstar Digital: Basic Computer Skills Course'] = {'link': 'https://nsdiglit.org/practice-computers', 'taken': true};
+    courses['Northstar Digital: Email Course'] = {'link': 'https://assessment.digitalliteracyassessment.org/email/practice/understanding-email', 'taken': false};
+    courses['NorthStar Digital: Word Course'] = {'link': 'https://nsdiglit.org/ms-word', 'taken':true};
+    courses['NorthStar Digital: Career Search Skills'] = {'link': 'https://nsdiglit.org/career-search-skills', 'taken': true};
 
+    // Count the number of courses taken
+    const takenCoursesCount = Object.values(courses).filter(course => course.taken).length;
+
+    // Calculate the fraction of courses taken
+    const fractionTaken = takenCoursesCount / Object.keys(courses).length;
     return (
         <div class="StudentHome">
             <h2>Welcome, Student!</h2>
             <h3>Courses</h3>
+            <progress class="colored" id="progress" value={fractionTaken * 100} max="100" > ${fractionTaken * 100}% </progress>
             <ul>
-                
-            {Object.entries(courses).map(([course, link], index) => (
-                <li key={index}>{course}: <a href={link}>{link}</a></li>
+            {Object.entries(courses).map(([course, value], index) => (
+                <li key={index}><a href={value.link}>{course}</a></li>
             ))}
             </ul>
+            
         </div>
     )
 }
